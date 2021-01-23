5b53b34e97898de7af6f34135d33b8699fbb4040
Edward Hsu <st89382000@gmail.com>
2021-01-24 02:27:14 +0800

    feat: Add NSwag pcakage and add project ref

---
+++ src/EdwardSolutionTemplate.API/Extensions/NSwagExtension.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EdwardSolutionTemplate.API.Extensions
+{
+    public static class NSwagExtension
+    {
+        public static IServiceProvider UseNswag(this IServiceProvider sp)
+        {
+            return sp;
+        }
+    }
+}
--- src/EdwardSolutionTemplate.API/Startup.cs
+++ src/EdwardSolutionTemplate.API/Startup.cs
@@ -17,6 +17,8 @@ namespace EdwardSolutionTemplate.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers()
+                .AddControllersAsServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
